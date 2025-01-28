7054414130d38de086bc16188aaa1a507becc7d2
Jishnu Suresh <jishnusuresh307@gmail.com>
2025-01-28 15:21:57 +0530

    database

--- Flask Demo/main.py
+++ Flask Demo/main.py
@@ -1,7 +1,9 @@
 
-from flask import Flask , render_template , request
+from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
 db = SQLAlchemy()
 #configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Event-Registration.db"
@@ -14,18 +16,40 @@ class Participant(db.Model):
     Email=db.Column(db.String,unique=True,nullable=False)
     Phone_no=db.Column(db.Integer,unique=True,nullable=False)
     Events=db.Column(db.PickleType,unique=False,nullable=False)
+    
 
 with app.app_context():
     db.create_all()
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/register")
+@app.route("/register",methods=["GET","POST"])
 def register():
+    if request.method=="POST":
+      name=request.form["name"]
+      email=request.form["email"]
+      phone=request.form["phone"]
+      #print(request.form)
+      events=[]
+      if request.form.get("event1"):
+        events.append(request.form.get("event1"))
+      if request.form.get("event2"):
+        events.append(request.form.get("event2"))
+      if request.form.get("event3"):
+        events.append(request.form.get("event3"))
+    #   print(name,email,phone,events) 
+      participant=Participant(Name=name,Email=email,Phone_no=phone,Events=events)
+      db.session.add(participant)
+      db.session.commit()
+      data="Registration Successful" 
+      return render_template("home.html",data=data, name=name, email=email, phone=phone, events=events)
     return render_template("register.html")
+
 
 
 if __name__=="__main__":
     app.run(debug=True)
+
